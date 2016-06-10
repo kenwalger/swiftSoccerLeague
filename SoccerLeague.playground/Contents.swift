7974576caa0aab47fb14dc06b1252a46d41708a9
Ken Alger <kenalger@Kens-MacBook-Pro.local>
2016-06-09 23:00:46 -0700

    code formatting

--- SoccerLeague.playground/Contents.swift
+++ SoccerLeague.playground/Contents.swift
@@ -192,7 +192,8 @@ func assignPlayers(playerGroup: [[Any]]) {
 
 func printLetters() {
     for player in players {
-        print("Hello \(player["guardian"]!): \n\(player["name"]!) has been chosen " + "to play on the \(teams[player["team"] as! Int]["name"]!) team." +
+        print("Hello \(player["guardian"]!): \n\(player["name"]!) has been chosen " +
+            "to play on the \(teams[player["team"] as! Int]["name"]!) team." +
             " \nThe first practice will be at " +
             "\(teams[player["team"] as! Int]["practiceTime"]!). \n\n")
     }
